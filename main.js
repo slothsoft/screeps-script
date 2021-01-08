6a55f021d73367a5446b4932874a04777536b53c
slothsoft <admin@slothsoft.de>
2021-01-08 16:22:24 +0100

    add basic tower draft so at least it works for now

--- main.js
+++ main.js
@@ -4,6 +4,25 @@ var baseManager = require('manager.base');
 
 module.exports.loop = function () {
 
+    var tower = Game.getObjectById('5ff55a31a435f9cd0078311a');
+    if(tower) {
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        } else {
+            
+            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
+            if(closestDamagedStructure) {
+                tower.repair(closestDamagedStructure);
+            }
+            
+        }
+        
+    }
+    
     // delete the memory of deceased creeps 
 
     for(var name in Memory.creeps) {
@@ -31,4 +50,4 @@ fetchOldestCreep = function (baseName) {
         console.log("Oldest creep: " + oldestCreep.name + " (" + oldestCreep.ticksToLive + " ttl)");
     else 
         console.log("No creep found.");
-}
+};
